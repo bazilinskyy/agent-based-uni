7bdf3884591aa8d59e2a1228e653bea468405b37
Pavlo Bazilinskyy <pavlo.bazilinskyy@gmail.com>
2014-01-28 21:03:32 +0200

    cleanup

--- src/model/Department.py
+++
@@ -1,6 +0,0 @@
-class Department():
-	__doc__ = "Department (part of faculty)"
-
-	def __init__(self, name, faculty):
-		self.name = name
-		self.faculty = faculty
--- src/model/Faculty.py
+++
@@ -1,5 +0,0 @@
-class Faculty():
-	__doc__ = "Faculty"
-	
-	def __init__(self, name):
-		self.name = name
